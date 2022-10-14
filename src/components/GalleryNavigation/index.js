286e91d12367b7619154b6409118cf7321098146
AA Student <89243713+jason330@users.noreply.github.com>
2022-10-14 16:23:42 -0700

    Add Navlinks

--- src/components/GalleryNavigation/index.js
+++ src/components/GalleryNavigation/index.js
@@ -1,7 +1,16 @@
+import { NavLink } from "react-router-dom";
+
 function GalleryNavigation({ galleries }) {
   console.log(galleries);
+  const galleryList = galleries.map(gallery => {
+    return <li><NavLink to={`/galleries/${gallery.id}`} key={gallery.id}>{gallery.name}</NavLink></li>
+  })
   return (
-    <h1>Galleries</h1>
+    <nav>
+      <h1>Galleries</h1>
+      <NavLink to="/">Home</NavLink>
+      <ul>{galleryList}</ul>
+    </nav>
   )
 }
 
